38c75ada3496e50c46079d3f5841aa4243ccd4d4
Ryan <rcadwell91@gmail.com>
2025-12-04 17:27:49 -0700

    Updated main.py and created logger.py and constants.py

--- main.py
+++ main.py
@@ -1,9 +1,28 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from logger import log_state
+
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Asteroids")
+
     print("Starting Asteroids with pygame version: 2.6.1")
     print("Screen width: 1280")
     print("Screen height: 720")
+
+    log_state()
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                return
+
+    screen.fill("black")
+
+
+    pygame.display.flip()
 
 
 if __name__ == "__main__":
